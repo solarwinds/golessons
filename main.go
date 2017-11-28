9ab16584660b708d65f9293ddf6da60698f7f9a3
Trevor Rosen <trevor@catapult-creative.com>
2017-11-28 10:18:08 -0600

    Add basic flag example

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	// flag is a package from the standard library for parsing CLI flags
+	"flag"
+)
+
+// friendly is a variable that is in scope everywhere in the main package
+var friendly bool
+
+// init is a special function like main and is guaranteed to run before main
+func init() {
+	// This sets up a boolean flag, held in the `friendly` variable so that it's accessible everywhere
+	flag.BoolVar(&friendly, "friendly", false, "whether to print out a greeting")
+	flag.Parse()
+}
+
 
 func main() {
-	fmt.Println("Hello, Gophers!")
+	if friendly {
+		fmt.Println("Hello, Gophers!")
+	}else{
+		fmt.Println("Goodbye, Gophers.")
+	}
 }
