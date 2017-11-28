3700266990f9c4939e4d1660ec0d47ab364f2668
Trevor Rosen <trevor@catapult-creative.com>
2017-11-28 13:59:38 -0600

    Add test and extend Makefile

--- Makefile
+++ Makefile
@@ -1,6 +1,11 @@
-.PHONY: build
+.PHONY: build test
+
+excluding_vendor := $(shell go list ./... | grep -v /vendor/)
 
 default: build
 
 build:
 	go build -i
+
+test:
+	go test -v $(excluding_vendor)
---
+++ web/web_test.go
@@ -0,0 +1,21 @@
+package web
+
+import "testing"
+
+// TestMetric_MillisecondTimestamp is run automatically because it starts with "Test"
+func TestMetric_MillisecondTimestamp(t *testing.T) {
+	in := int64(1511898670850117031)
+	out := int64(1511898670850117)
+
+	metric := &Metric{
+		Name:      "test",
+		Value:     2.71828,
+		Timestamp: in,
+	}
+
+	result := metric.MillisecondTimestamp()
+
+	if result != out {
+		t.Errorf("expected %d to equal %d but it didn't", result, out)
+	}
+}
