03fa53bb9421fea92209213ea07752a76de35a72
Trevor Rosen <trevor@catapult-creative.com>
2017-11-28 10:22:48 -0600

    Add int flag, value interpolation

--- main.go
+++ main.go
@@ -9,10 +9,15 @@ import (
 // friendly is a variable that is in scope everywhere in the main package
 var friendly bool
 
+// port will hold the value of the TCP port we wish to bind to
+var port int
+
 // init is a special function like main and is guaranteed to run before main
 func init() {
 	// This sets up a boolean flag, held in the `friendly` variable so that it's accessible everywhere
 	flag.BoolVar(&friendly, "friendly", false, "whether to print out a greeting")
+
+	flag.IntVar(&port, "port", 7777, "the TCP port to bind to")
 	flag.Parse()
 }
 
@@ -23,4 +28,7 @@ func main() {
 	}else{
 		fmt.Println("Goodbye, Gophers.")
 	}
+
+	portString := fmt.Sprintf(":%d", port)
+	fmt.Printf("binding to %s\n", portString)
 }
