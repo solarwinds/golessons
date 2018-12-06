0f4de16e9f466962839852d22c5d15573ce07b00
chris erway <cce@solarwinds.cloud>
2018-12-06 09:59:16 +0100

    Run go fmt ./...

--- web/web.go
+++ web/web.go
@@ -6,12 +6,12 @@ import "net/http"
 // function with this signature can be *cast* to an http.Handler and therefore used by
 // the http package's default multiplexer to handle requests.
 func GetHello(verbose bool) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if verbose {
 			w.Write([]byte("Here's some awesome body content!\n"))
 			// good to put these here to ensure that this is the end of execution
 			return
-		}else{
+		} else {
 			w.WriteHeader(http.StatusAccepted)
 		}
 	})
